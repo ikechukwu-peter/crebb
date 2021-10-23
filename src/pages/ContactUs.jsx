56f28044587df616ca3f416940273d3548e645ab
ikechukwu-peter <pitiyke@gmail.com>
2021-10-23 17:58:29 +0100

    fix: corrected grammar

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -14,7 +14,7 @@ function ContactUs() {
             <div className="form_head">
                 <div className="form_subhead">
                     <form className="main_form">
-                        <h2 className="intro_text"> Hey!! Shoot 🔫 us email </h2>
+                        <h2 className="intro_text"> Hey!! Shoot 🔫 us an email </h2>
                         <label className="input_label" for="name">Name </label>
                         <input className="_input" type="text" id="name" placeholder="Enter your name" />
                         <label className="input_label" for="email">Email </label>
