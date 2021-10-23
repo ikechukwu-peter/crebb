d6dfcf3087ec93674e5f0dea563d7914f372c814
ikechukwu-peter <pitiyke@gmail.com>
2021-10-23 03:47:10 +0100

    feat: created homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import react from 'react'
-import Navbar from './pages/Navbar'
+import Home from './pages/Home'
 
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Home />
     </div>
   )
 }
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,46 +1,17 @@
 import React from 'react'
+import  '../styles/navbar.css';
 
 function Navbar() {
     return (
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid justify-content-between">
-    <a class="navbar-brand" href="#" d>Crebb</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Create a Session</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">View Session</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Login</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Signup</a>
-        </li>
-        
-      </ul>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link "  href="#">Contact Us</a>
-        </li>     
-        
-      </ul>
-    </div>
-    </div>
-
-  </div>
-</nav>
-   
+             <div className="navbar">
+            <strong className="logo">Crebb</strong>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Book a session</a></li>
+                <li><a href="#">Sign Up</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>       
+         </div>           
     )
 }
 
