c28a1db685e1b1ee7f2300e7c2f5d0a358cd60a4
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2021-10-25 20:05:57 +0100

    fetched

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,84 +1,74 @@
-import React from "react";
+import React, { useState } from 'react'
 import { NavLink } from "react-router-dom";
-import "../styles/navbar.css";
-import { useState } from 'react';
+import { FaAlignCenter } from 'react-icons/fa'
+import '../styles/navbar.css';
 
 function Navbar() {
-    const activeStyle = { color: "#009688" };
-   
-    
-        const [name, setName] = useState("none");
-    
-    const handleMenu = () => {
-       setName("grid");
+    const [toggle, setToggle] = useState("false")
+
+    const toggler = () => {
+        setToggle(!toggle);
     };
 
-  return (
-    // Authenticated Link
-    // <div className="navbar">
-    //     <strong className="logo">Crebb</strong>
-    //     <ul>
-    //         <li>
-    //              <NavLink exact to="/" activeStyle={activeStyle}>Home</NavLink>
-    //
-    //         </li>
-    //          <li>
-    //          <NavLink exact to="/contact-us" activeStyle={activeStyle}>Contact Us</NavLink>
-    //
-    //          </li>
-    //          <li>
-    //          <NavLink exact to="/see-all-session" activeStyle={activeStyle}>See Your Sessions</NavLink>
-    //
-    //          </li>
-    //         <li>
-    //         <NavLink exact to="/book-a-session" activeStyle={activeStyle}>Book A Session</NavLink>
-    //
-    //         </li>
-    //         <li>   <NavLink exact to="/login" activeStyle={activeStyle}>Log Out</NavLink>
-    //
-    //         </li>
-    //      </ul>
-    // </div>
+     const activeStyle = { color: '#009688' };
 
-    //Guest Link
+    return (
+        // Authenticated Link
+         <div>
+        <div className="menu-bar " onClick={toggler}><FaAlignCenter /></div>
+        <div className="navbar">
+            <strong className="logo">Crebb</strong>
+            <ul className={toggle ? "nav-links" : "open"}>
+                <li onClick={toggler}>  
+                     <NavLink exact to="/" activeStyle={activeStyle}>Home</NavLink>
 
-    <div className="navbar">
-      <div className="mobile">
-        <strong className="logo">Crebb</strong>
-        {/* HAMBURGER MENU >>>*/}{" "}
-        <img
-          src="src\assets\bars-solid.svg"
-          alt=""
-          className="img"
-            onClick={handleMenu}
-        />
-      </div>
+                </li>
+                 <li onClick={toggler}>
+                 <NavLink exact to="/contact-us" activeStyle={activeStyle}>Contact Us</NavLink>
 
-      <ul id="unordered" style={{display:name}}>
-        <li>
-          <NavLink exact to="/" activeStyle={activeStyle}>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink exact to="/contact-us" activeStyle={activeStyle}>
-            Contact Us
-          </NavLink>
-        </li>
-        <li>
-          {" "}
-          <NavLink exact to="/login" activeStyle={activeStyle}>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink exact to="/register" activeStyle={activeStyle}>
-            Sign Up
-          </NavLink>
-        </li>
-      </ul>
-    </div>
-  );
+                 </li>
+                 <li onClick={toggler}>
+                 <NavLink exact to="/see-all-sessions" activeStyle={activeStyle}>Bookings</NavLink>
+
+                 </li>
+                <li onClick={toggler}>
+                <NavLink exact to="/book-a-session" activeStyle={activeStyle}>Book</NavLink>
+
+                </li>
+                <li onClick={toggler}>   <NavLink exact to="/login" activeStyle={activeStyle}>Log Out</NavLink>
+
+                </li>
+             </ul>
+        </div>
+        </div>
+
+        //Guest Link
+        // <div>
+        //     <div className="menu-bar " onClick={toggler}><FaAlignCenter /></div>
+        //     <div className="navbar">
+        //         <strong className="logo">Crebb</strong>
+        //         <ul className={toggle ? "nav-links" : "open"}  >
+        //             <li>
+        //                 <NavLink exact to="/" activeStyle={activeStyle} onClick={toggler}>Home</NavLink>
+
+        //             </li>
+        //             <li onClick={toggler}>
+        //                 <NavLink exact to="/contact-us" activeStyle={activeStyle} >Contact Us</NavLink>
+
+        //             </li>
+        //             <li onClick={toggler}>   <NavLink exact to="/login" activeStyle={activeStyle} >Login</NavLink>
+
+        //             </li>
+        //             <li onClick={toggler}>
+        //                 <NavLink exact to="/register" activeStyle={activeStyle}>Sign Up</NavLink>
+
+        //             </li>
+        //         </ul>
+        //     </div>
+        // </div>
+
+    )
+
 }
 
 export default Navbar;
