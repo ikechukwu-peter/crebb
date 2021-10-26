acda3018df5eca95bf9553ce273a10efc9f18e52
ikechukwu-peter <pitiyke@gmail.com>
2021-10-26 11:14:59 +0100

    feat: added log and favicon

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { NavLink } from "react-router-dom";
 import { FaAlignCenter } from 'react-icons/fa'
+import logo from '../assets/crebb.png'
 import '../styles/navbar.css';
 
 function Navbar() {
@@ -10,36 +11,39 @@ function Navbar() {
         setToggle(!toggle);
     };
 
-     const activeStyle = { color: '#009688' };
+    const activeStyle = { color: '#009688' };
 
     return (
         // Authenticated Link
-         <div>
-        <div className="menu-bar " onClick={toggler}><FaAlignCenter /></div>
-        <div className="navbar">
-            <strong className="logo">Crebb</strong>
-            <ul className={toggle ? "nav-links" : "open"}>
-                <li onClick={toggler}>  
-                     <NavLink exact to="/" activeStyle={activeStyle}>Home</NavLink>
+        <div>
+            <div className="menu-bar " onClick={toggler}><FaAlignCenter /></div>
+            <div className="navbar">
+                <NavLink  exact to="/" activeStyle={activeStyle}>
+                    <img className="logo" src={logo} alt="Crebb logo" title="Crebb logo" />
+                </NavLink>
 
-                </li>
-                 <li onClick={toggler}>
-                 <NavLink exact to="/contact-us" activeStyle={activeStyle}>Contact Us</NavLink>
+                <ul className={toggle ? "nav-links" : "open"}>
+                    <li onClick={toggler}>
+                        <NavLink exact to="/" activeStyle={activeStyle}>Home</NavLink>
 
-                 </li>
-                 <li onClick={toggler}>
-                 <NavLink exact to="/see-all-sessions" activeStyle={activeStyle}>Bookings</NavLink>
+                    </li>
+                    <li onClick={toggler}>
+                        <NavLink exact to="/contact-us" activeStyle={activeStyle}>Contact Us</NavLink>
 
-                 </li>
-                <li onClick={toggler}>
-                <NavLink exact to="/book-a-session" activeStyle={activeStyle}>Book</NavLink>
+                    </li>
+                    <li onClick={toggler}>
+                        <NavLink exact to="/see-all-sessions" activeStyle={activeStyle}>Bookings</NavLink>
 
-                </li>
-                <li onClick={toggler}>   <NavLink exact to="/login" activeStyle={activeStyle}>Log Out</NavLink>
+                    </li>
+                    <li onClick={toggler}>
+                        <NavLink exact to="/book-a-session" activeStyle={activeStyle}>Book</NavLink>
 
-                </li>
-             </ul>
-        </div>
+                    </li>
+                    <li onClick={toggler}>   <NavLink exact to="/login" activeStyle={activeStyle}>Log Out</NavLink>
+
+                    </li>
+                </ul>
+            </div>
         </div>
 
         //Guest Link
