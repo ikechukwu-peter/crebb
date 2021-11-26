11aab7b049bed1a416232370d2c2b31952be9294
ikechukwu-peter <pitiyke@gmail.com>
2021-11-26 12:14:33 +0100

    fix: removed wrong import

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar';
 import Footer from './Footer';
-import LoginForm  from '../components/LoginForm';
+import LoginForm from '../components/loginForm';
 
 
 
